e0519f409211e0b30b00f2d7ffe6227ce5547d3e
“Houssine” <person.zen15@gmail.com>
2022-03-12 23:45:34 +0100

    Add more features to getAllProduct

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,24 @@
 const Product = require("./../model/productModel");
 const productRoute = require("express").Router();
 
+// Get All Products
 productRoute.get("/products", async (req, res) => {
   try {
-    const data = await Product.find({});
+    // Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((field) => delete queryObj[field]);
+    const queryString = JSON.stringify(queryObj).replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+    const newQueryObj = JSON.parse(queryString);
+
+    // Execute Query
+    const data = await Product.find(newQueryObj);
     res.status(200).json({
       status: "Success",
+      count: data.length,
       products: data,
     });
   } catch (err) {
@@ -15,7 +28,7 @@ productRoute.get("/products", async (req, res) => {
     });
   }
 });
-
+// Create Product
 productRoute.post("/products", async (req, res) => {
   try {
     const data = req.body;
@@ -28,6 +41,43 @@ productRoute.post("/products", async (req, res) => {
     res.status(400).json({
       status: "Add Fail",
       errorLog: err,
+    });
+  }
+});
+// Delete Product
+productRoute.delete("/products/:id", async (req, res) => {
+  try {
+    console.log(`We are Deleting: ${req.params.id}`);
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.status(201).json({
+      status: "Delete Success",
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Delete Fail",
+      errorLog: err.message,
+    });
+  }
+});
+
+// Update Product
+productRoute.patch("/products/:id", async (req, res) => {
+  try {
+    console.log(`We are Updating: ${req.params.id}`);
+    const { id } = req.params;
+    const newData = req.body;
+    const updatedProduct = await Product.findOneAndUpdate(id, newData, {
+      new: true,
+    });
+    res.status(201).json({
+      status: "Update Success",
+      updatedProduct,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Update Fail",
+      errorLog: err.message,
     });
   }
 });
